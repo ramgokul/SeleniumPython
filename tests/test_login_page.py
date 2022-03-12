d1fd5180d812e1d42bfbcb82e6864c3ad8a3c484
ramnath1109@gmail.com <CiTrIx_e1008>
2022-03-12 14:29:09 +0530

    Parametrize test input data

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -3,6 +3,15 @@ from assertpy import assert_that
 
 from pages.login_page import LoginPage
 
+# ********************** Invalid login credentials to parametrize ************************
+example_params = [("tomsmith", "test"), ("abcd", "xyz")]
+
+
+@pytest.fixture(params=example_params)
+def param_loop(request):
+    return request.param
+# ********************** Invalid login credentials to parametrize ************************
+
 
 @pytest.mark.regression
 class TestLoginPage:
@@ -13,8 +22,7 @@ class TestLoginPage:
         is_login_success = login.verify_login_success()
         assert_that(is_login_success).is_true()
 
-    def test_invalid_credentials(self, login):
-        login.with_("tomsmith", "test")
+    def test_invalid_credentials(self, login, param_loop):
+        login.with_(param_loop[0], param_loop[1])
         is_login_failure_msg_displayed = login.verify_login_failure()
         assert_that(is_login_failure_msg_displayed).is_true()
-
